3adbe1f64e21421c8fecf9a9a52a2e82adccb88b
kostyarus6715 <118332856+kostyarus6715@users.noreply.github.com>
2023-03-04 14:22:56 +0700

    Add files via upload

---
+++ 2.2.2/ConsoleApp2/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Xml.Linq;
+
+namespace ConsoleApp1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Train trains = new Train();
+            for (int i = 0; i >= 0; i++)
+            {
+                Console.Clear();
+                Console.Write("Выберите пункт: \n" +
+                              "1 - Добавить информацию о поезде \n" +
+                              "2 - Посмотреть номер поезда \n" +
+                              "3 - Посмотреть информацию по номеру поезда \n" +
+                              "4 - Изменение информации\n" +
+                              "5 - Выход\n");
+
+                              int a = Convert.ToInt32(Console.ReadLine());
+                switch (a)
+                {
+                 case 1:
+                     Console.Clear();
+                     trains.addAlL();
+                     trains.trainInfo();
+                     Console.Write("\n");
+                     break;
+                 case 2:
+                     Console.Clear();
+                     Console.Write($"Номер введёного поезда: {trains.num} \n");
+                     Console.Write("Нажмите Enter, чтобы продолжить");
+                     Console.ReadKey();
+                     break;
+                 case 3:
+                     Console.Clear();
+                     trains.tNumber(trains);
+                     break;
+                 case 4:
+                     Console.Clear();
+                     Console.WriteLine("Выберите пункт \n" +
+                                   "1 - Изменить пункт назначения \n" +
+                                   "2 - Изменить номер поезда \n" +
+                                   "3 - Изменить время отправления");
+                     int b = Convert.ToInt32(Console.ReadLine());
+                     switch (b)
+                     {
+                         case 1:
+                             Console.Clear();
+                             Console.Write("Введите пункт назначения: ");
+                             trains.punkt = Console.ReadLine();
+                             trains.trainInfo();
+                             break;
+                         case 2:
+                             Console.Clear();
+                             Console.Write("Введите номер поезда: ");
+                             trains.num = Console.ReadLine();
+                             trains.trainInfo();
+                             break;
+                         case 3:
+                             Console.Clear();
+                             Console.Write("Введите время отправления: ");
+                             trains.time = Console.ReadLine();
+                             trains.trainInfo();
+                             break;  
+                     }
+                     break;
+                 case 5:
+                     Console.Clear();
+                     Console.Write("Пока-пока");
+                     Environment.Exit(0);
+                     break;
+                }
+            }
+        }
+    }
+}
---
+++ 2.2.2/ConsoleApp2/Train.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace ConsoleApp1
+{
+    public class Train
+    {
+        public string punkt { get; set; } //Пункт назначения
+        public string num { get; set; } //Номер поезда
+        public string time { get; set; } //Время отправления
+
+        public void trainInfo()
+        {
+            Console.WriteLine($"Пункт назначения: {punkt} \n" +
+                              $"Номер поезда: {num} \n" +
+                              $"Время отправления: {time}\n" +
+                              "Нажмите Enter, чтобы продолжить");
+            Console.ReadKey();
+            Console.Clear();
+        }
+
+        public void addAlL()
+        {
+            Console.Write("Введите пункт назначения: ");
+            punkt = Console.ReadLine();
+
+            Console.Write("Введите номер поезда: ");
+            num = Console.ReadLine();
+
+            Console.Write("Введите время отправления: ");
+            time = Console.ReadLine();
+            Console.Clear();
+            Console.Write("\n");
+        }
+
+        public void tNumber(Train trains)
+        {
+            Console.Write("Введите номер нужного вам поезда : ");
+            String number = Console.ReadLine();
+            Console.Clear();
+            for (int j = 0; j < 1; j++)
+            {
+                if (number == num)
+                {
+                    Console.Write("\nИнформация о вашем поезде\n");
+                    trains.trainInfo();
+                }
+                else
+                {
+                    Console.Write("Такого поезда нету");
+                    Console.ReadKey();
+                }
+            }
+        }
+    }
+}
