2fa02defad1f46594c1fddc208c63a162c8bfcd8
kostyarus6715 <118332856+kostyarus6715@users.noreply.github.com>
2023-03-04 14:21:00 +0700

    Add files via upload

---
+++ 2.2.1/ConsoleApp1/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Xml.Linq;
+
+namespace ConsoleApp1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Student stud = new Student();
+            for (int i = 0; i >= 0; i++)
+            {
+                Console.Write("Выберите пункт: \n" +
+                              "1 - Добавить информацию \n" +
+                              "2 - Просмотр \n" +
+                              "3 - Изменение \n");
+
+                              int a = Convert.ToInt32(Console.ReadLine());
+                switch (a)
+                {
+                 case 1:
+                     addAll(stud);
+                     stud.studenInfo();
+                     Console.Write("\n");
+                     break;                    
+                    case 2: 
+                        stud.studenInfo();
+                     Console.Write("\n");
+                     break;
+
+                 case 3:
+                     Console.WriteLine("Выберите пункт \n" +
+                                   "1 - Изменить фамилию \n" +
+                                   "2 - Изменить дату \n" +
+                                   "3 - Изменить номер группы \n" +
+                                   "4 - Изменить успеваемость ");
+                     int b = Convert.ToInt32(Console.ReadLine());
+                     switch (b)
+                     {
+                         case 1:
+                             Console.Write("Введите фамилию: ");
+                             stud.sName = Console.ReadLine();
+                             stud.studenInfo();
+                             break;
+                         case 2:
+                             Console.Write("Введите дату: ");
+                             stud.date = Console.ReadLine();
+                             stud.studenInfo();
+                             break;
+                         case 3:
+                             Console.Write("Введите номер группы: ");
+                             stud.numGroups = Convert.ToInt32(Console.ReadLine());
+                             stud.studenInfo();
+                             break;
+                         case 4:
+                             stud.uspev = new int[5];
+                             for (int j = 0; j < stud.uspev.Length; j++)
+                             {
+                                 Console.Write("Введите успеваемость: ");
+                                 stud.uspev[j] = int.Parse(Console.ReadLine());
+                             }
+                             stud.studenInfo();
+                             break; }
+                     break;
+                }
+            }
+        }
+
+       static void addAll(Student stud)
+        {
+            Console.Write("Введите фамилию: ");
+            stud.sName = Console.ReadLine();
+            
+            Console.Write("Введите дату: ");
+            stud.date = Console.ReadLine();
+            
+            Console.Write("Введите номер группы: ");
+            stud.numGroups = Convert.ToInt32(Console.ReadLine());
+            
+            stud.uspev = new int[5];
+            for (int j = 0; j < stud.uspev.Length; j++)
+            {
+               Console.Write("Введите успеваемость: ");
+               stud.uspev[j] = int.Parse(Console.ReadLine());
+            }
+            
+            Console.Write("\n");
+        }
+    }
+}
---
+++ 2.2.1/ConsoleApp1/Student.cs
@@ -0,0 +1,26 @@
+﻿
+using System.Collections.Generic;
+using System.Globalization;
+namespace ConsoleApp1;
+
+public class Student
+{
+public string sName { get; set; } //Фамилия
+public string date { get; set; } //Дата
+public int numGroups { get; set; } //Номер группы
+public int[] uspev { get; set; } //Успеваемость
+
+public void studenInfo()
+{
+    Console.WriteLine($"Фамилия: {sName} \n" +
+                        $"Дата рождения: {date} \n" +
+                        $"Номер группы: {numGroups}");
+    Console.Write( $"Успеваемость: ");
+    foreach (var i in uspev)
+    {
+        Console.Write(i + " ");
+    }
+    Console.Write("\n");
+    Console.ReadKey();
+}
+}
