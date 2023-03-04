7d4edad9d960f1edbbcdcaf8d78d5cf4286f2118
kostyarus6715 <118332856+kostyarus6715@users.noreply.github.com>
2023-03-04 14:25:13 +0700

    Add files via upload

---
+++ 2.2.3/ConsoleApp1/Chisla.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace ConsoleApp1
+{
+    public class Chisla
+    {
+        public int chislo1 { get; set; } //Число 1
+        
+        public int chislo2 { get; set; } //Число 2
+
+        public void edit1()
+        {
+            Console.Write("Введите первое число: ");
+            chislo1 = Convert.ToInt32(Console.ReadLine());
+            cInfo();
+        }
+        public void edit2()
+        {
+            Console.Write("Введите второе число: ");
+            chislo2 = Convert.ToInt32(Console.ReadLine());
+            cInfo();
+        }
+        public void addAll()
+        {
+            Console.Write("Введите первое число: ");
+            chislo1 = Convert.ToInt32(Console.ReadLine());
+            
+            Console.Write("Введите второе число: ");
+            chislo2 = Convert.ToInt32(Console.ReadLine());
+        }
+
+        public void cInfo()
+        {
+            Console.Write($"Первое число: {chislo1}"+
+                          $"\nВторое число: {chislo2}"+
+                          $"\n Нажмите Enter, чтобы продолжить");
+            Console.ReadKey();
+        }
+
+        public void summa()
+        {
+            int a = chislo1 + chislo2;
+            Console.Write($"Сумма чисел {chislo1} + {chislo2} = {a}\n"+
+                          "Нажмите Enter, чтобы продолжить");
+            Console.ReadKey();
+        }
+
+        public void sravn()
+        {
+            Console.Write($"Из введёных чисел {chislo1} и {chislo2}");
+            if (chislo1 > chislo2)
+            {
+                Console.Write($"\n{chislo1} является наибольшим");
+            }
+            else if (chislo2 > chislo1)
+            {
+                Console.Write($"\n{chislo2} является наибольшим");
+            }
+            else
+            {
+                Console.Write($"\nВведёные числа равны");
+            }
+            Console.ReadKey();
+        }
+    }
+    
+}
---
+++ 2.2.4/ConsoleApp1/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int x = 10; //стандартное значение
+            int y = x;
+            for (int i = 0; i >= 0; i++)
+            {
+                Console.Write("Выберите пункт: \n" +
+                              "1 - Увеличить значение на 1 \n" +
+                              "2 - Уменьшить значение на 1 \n" +
+                              "3 - Задать счётчику своё значение \n" +
+                              "4 - Вернуть значение по умолчанию\n" +
+                              "5 - Посмотреть текущее значение\n" +
+                              "6 - Выход \n");
+                int a = Convert.ToInt32(Console.ReadLine());
+                switch (a)
+                {
+                    case 1:
+                        plusone();
+                        break;
+                    case 2:
+                        minusone();
+                        break;
+                    case 3:
+                        Console.Clear();
+                        Console.Write("Введите своё значение: ");
+                        y = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 4:
+                        Console.Clear();
+                        Console.Write($"Значение установлено по умолчанию = {x}\n");
+                        y = x;
+                        break;
+                    case 5:
+                        Console.Clear();
+                        Console.Write($"Текущее значение счётчика = {y} \n");
+                        break;
+                    case 6:
+                        Console.Clear();
+                        Console.Write("Пока-пока");
+                        Environment.Exit(0);
+                        break;
+                }
+                void plusone()
+                {
+                    Console.Clear();
+                    Console.Write($"Результат = {y+=1}\n");
+                }
+
+                void minusone()
+                {
+                    Console.Clear();
+                    Console.Write($"Результат = {y-=1}\n");
+                }
+            }
+        }
+    }
+}
+
