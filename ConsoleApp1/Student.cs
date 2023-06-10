71512da074d93e0ea65fd944c2982ec3674935a3
kostyarus6715 <118332856+kostyarus6715@users.noreply.github.com>
2023-06-10 17:42:49 +0700

    2.1

---
+++ ConsoleApp1/Student.cs
@@ -0,0 +1,23 @@
+﻿namespace ConsoleApp1;
+
+public class Student
+{
+public string? SName { get; set; } //Фамилия
+public string? Date { get; set; } //Дата
+public int NumGroups { get; set; } //Номер группы
+public int[] AcademicPerformance { get; set; } //Успеваемость
+
+public void StudentInfo()
+{
+    Console.WriteLine($"Фамилия: {SName} \n" +
+                        $"Дата рождения: {Date} \n" +
+                        $"Номер группы: {NumGroups}");
+    Console.Write( $"Успеваемость: ");
+    foreach (var i in AcademicPerformance)
+    {
+        Console.Write(i + " ");
+    }
+    Console.Write("\n");
+    Console.ReadKey();
+}
+}
