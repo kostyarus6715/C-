9fda6bc854822f912f0604d26ae788bbeba82a72
kostyarus6715 <118332856+kostyarus6715@users.noreply.github.com>
2023-06-10 17:50:08 +0700

    Add files via upload

---
+++ 2.2.3/Numbers.cs
@@ -0,0 +1,53 @@
+﻿namespace ConsoleApp3
+{
+    public class Numbers
+    {
+        public int Chislo1 { get; set; } //Число 1
+        
+        public int Chislo2 { get; set; } //Число 2
+
+        public void AddAll()
+        {
+            Console.Write("Введите первое число: ");
+            Chislo1 = Convert.ToInt32(Console.ReadLine());
+            
+            Console.Write("Введите второе число: ");
+            Chislo2 = Convert.ToInt32(Console.ReadLine());
+        }
+
+        public void NumberInfo()
+        {
+            Console.Write($"Первое число: {Chislo1}"+
+                          $"\nВторое число: {Chislo2}"+
+                          $"\n Нажмите Enter, чтобы продолжить");
+            Console.ReadKey();
+        }
+
+        public void Sum()
+        {
+            int a = Chislo1 + Chislo2;
+            Console.Write($"Сумма чисел {Chislo1} + {Chislo2} = {a}\n"+
+                          "Нажмите Enter, чтобы продолжить");
+            Console.ReadKey();
+        }
+
+        public void Comparison()
+        {
+            Console.Write($"Из введёных чисел {Chislo1} и {Chislo2}");
+            if (Chislo1 > Chislo2)
+            {
+                Console.Write($"\n{Chislo1} является наибольшим");
+            }
+            else if (Chislo2 > Chislo1)
+            {
+                Console.Write($"\n{Chislo2} является наибольшим");
+            }
+            else
+            {
+                Console.Write($"\nВведёные числа равны");
+            }
+            Console.ReadKey();
+        }
+    }
+    
+}
---
+++ 2.2.3/Program.cs
@@ -0,0 +1,73 @@
+﻿namespace ConsoleApp3
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Numbers numbers = new Numbers();
+            for (int i = 0; i >= 0; i++)
+            {
+                Console.Clear();
+                Console.Write("Выберите пункт: \n" +
+                              "1 - Добавить числа \n" +
+                              "2 - Посмотреть введёные числа \n" +
+                              "3 - Изменение чисел \n" +
+                              "4 - Найти сумму этих чисел \n" +
+                              "5 - Найти наибольше из чисел \n" +
+                              "6 - Выход \n");
+                int a = Convert.ToInt32(Console.ReadLine());
+                switch (a)
+                {
+                    case 1:
+                        Console.Clear();
+                        numbers.AddAll();
+                        break;
+                    case 2:
+                        Console.Clear();
+                        numbers.NumberInfo();
+                        break;
+                    case 3:
+                        Console.Clear();
+                        Console.Write("Выберите пункт \n"+
+                                          "1 - Изменить первое число\n"+
+                                          "2 - Изменить второе число\n"+
+                                          "3 - Изменить оба числа\n");
+                        int b = Convert.ToInt32(Console.ReadLine());
+                        switch (b)
+                        {
+                            case 1:
+                                Console.Clear();
+                                Console.Write("Введите первое число: ");
+                                numbers.Chislo1 = Convert.ToInt32(Console.ReadLine());
+                                numbers.NumberInfo();
+                                break;
+                            case 2:
+                                Console.Clear();
+                                Console.Write("Введите второе число: ");
+                                numbers.Chislo2 = Convert.ToInt32(Console.ReadLine());
+                                numbers.NumberInfo();
+                                break;
+                            case 3:
+                                Console.Clear();
+                                numbers.AddAll();
+                                break;
+                        }
+                        break;
+                        case 4:
+                            Console.Clear();
+                            numbers.Sum();
+                            break;
+                        case 5:
+                            Console.Clear();
+                            numbers.Comparison();
+                            break;
+                        case 6:
+                            Console.Clear();
+                            Console.Write("Пока-пока");
+                            Environment.Exit(0);
+                            break;
+                }
+            }
+        }
+    }
+}
