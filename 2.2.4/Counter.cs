34da1d50f8d383ba02df96bd7e19fa754bd86661
kostyarus6715 <118332856+kostyarus6715@users.noreply.github.com>
2023-06-10 17:50:41 +0700

    Add files via upload

---
+++ 2.2.4/Counter.cs
@@ -0,0 +1,34 @@
+﻿namespace ConsoleApp4
+{
+    internal class Counter
+    {
+
+        public int Count { get; set; }
+
+        public Counter()
+        {
+            Random rand = new Random();
+            Count = rand.Next(0, 10);
+
+        }
+        public Counter(int count)
+        {
+            this.Count = count;
+        }
+        public void Plus()
+        {
+            Count++;
+        }
+        public void Minus()
+        {
+            Count--;
+        }
+        public int GetCount
+        {
+            get { return Count; }
+        }
+
+
+
+    }
+}
---
+++ 2.2.4/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string? ing = "go";
+            Counter name;
+            Console.WriteLine("Выберите начало отсчёта:\n1 - По умолчанию\n2 - Произвольное");
+            int input = Convert.ToInt32(Console.ReadLine());
+
+            if (input == 1)
+            {
+
+                name = new Counter();
+
+            }
+            else
+            {
+                Console.WriteLine("Введите начало отсчёта");
+                input = Convert.ToInt32(Console.ReadLine());
+                name = new Counter(input);
+            }
+
+            Console.WriteLine("Нажмите клавишу \"Enter\" для прибавления к числу еденицы\n" +
+                    "Введите \"1\" чтобы отнять отнять еденицу\n" +
+                    "Введите\"2\" чтобы узнать текущее состояние счетчика\n" +
+                    "Введите\"3\" чтобы выйти из счетчика");
+
+            while (ing != "exit")
+            {
+                ing = Console.ReadLine();
+                if (ing == "3") break;
+
+                switch (ing)
+                {
+                    case "":
+                        name.Plus();
+                        break;
+                    case "1":
+                        name.Minus();
+                        break;
+                    case "2":
+                        int res = name.GetCount;
+                        Console.WriteLine("Текущее состояние счётчика: " + res);
+                        break;
+                }
+
+            }
+        }
+    }
+}
+
+
+
+
