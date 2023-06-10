05a032ca33974589685b7ce1d933fa19ed6801c5
kostyarus6715 <118332856+kostyarus6715@users.noreply.github.com>
2023-06-10 17:51:16 +0700

    Add files via upload

---
+++ 2.2.5/Complex.cs
@@ -0,0 +1,25 @@
+﻿namespace ConsoleApp5
+{
+
+    public class Person
+    {
+        public int Num1 { get; set; }
+
+        public int Num2 { get; set; }
+        
+        public Person()
+        {
+            Console.WriteLine("Введите первое число: ");
+            Num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите второе число: ");
+            Num2 = Convert.ToInt32(Console.ReadLine());
+        }
+
+        
+        ~Person()
+        {
+            Console.WriteLine($"{Num1} Объект удалён");
+            Console.WriteLine($"{Num2} Объект удалён");
+        }
+    }
+}
---
+++ 2.2.5/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace ConsoleApp5
+{
+    internal class Program
+    {
+        public static void Method()
+        {
+            Person tom = new Person();
+        }
+        static void Main()
+        {
+            Method();
+            GC.Collect();
+
+        }
+    }
+}
